62df164ae1ac0a02c571e76811430bf6c1fb89c7
ElliotWG <kingsteel5493@163.com>
2017-07-28 17:14:47 +0800

    Add files via upload

---
+++ welcome.java
@@ -0,0 +1,14 @@
+package test;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String greeting = "welcome to java !";
+System.out.println(greeting);
+for (int i = 0; i < greeting.length(); i++)
+	System.out.print("=");
+System.out.println();
+	}
+
+}
